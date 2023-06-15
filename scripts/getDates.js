2f4f1b454fec712ab05ff86c3347e462ca21194c
Elena Carey <theelenacarey@gmail.com>
2023-06-15 16:48:41 -0500

    Color changes and hamburger menu

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -6,3 +6,11 @@ yearBox.textContent = d.getFullYear()
 // date modified code
 var modified = document.getElementById("lastModified");
 modified.textContent = document.lastModified;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
