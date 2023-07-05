77387cd31184bba8fa06d0b8b289bccf7bc47a7d
Elena Carey <theelenacarey@gmail.com>
2023-07-05 09:58:54 -0500

    Finished adding responsive links.

--- scripts/links.js
+++ scripts/links.js
@@ -4,7 +4,34 @@ const linksURL = "https://elenalaree.github.io/wdd230/data/links.json";
 async function getLinks(){
     const resp = await fetch(linksURL);
     const data = await resp.json();
-    console.log(data)
+    displayLinks(data)
 }
 
 getLinks();
+const linkBox = document.getElementById("link-box");
+function displayLinks(weeks){
+    const weekArray = weeks.weeks
+    
+    for (let i = 0; i < weekArray.length; i++) {
+        const weekList = document.createElement('li');
+        let week = weekArray[i];
+        let weekName = `${week.week}: `;
+        let links =  week.links;
+        weekList.textContent = weekName;
+        for (let j = 0; j < links.length; j++) {
+            const linkEl = document.createElement('a');
+            const link = links[j];
+            const url = link.url;
+            const title = `${link.title} | `;
+            linkEl.setAttribute("href", url);
+            linkEl.textContent = title;
+            weekList.appendChild(linkEl);
+            
+            console.log(title)
+        }
+        
+        const element = weekArray[i];
+        linkBox.appendChild(weekList)
+        console.log(links)
+    }
+}
